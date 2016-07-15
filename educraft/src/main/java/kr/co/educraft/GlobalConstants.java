f71f46e8cf79dce3f7ea27c0907b1a4dbad220dc
Administrator <Administrator@redcore>
2016-07-15 20:32:30 +0900

    ^^

--- educraft/src/main/java/kr/co/educraft/GlobalConstants.java
+++ educraft/src/main/java/kr/co/educraft/GlobalConstants.java
@@ -1,7 +1,5 @@
 package kr.co.educraft;
 
 public class GlobalConstants {
-	//  MEMBER_TYPE(회원종류)
-	public static final String MEMBER_TYPE_ADMIN = "A"; //관리자
-	
+	public static final String MEMBER_TYPE_ADMIN = "A";	
 }
